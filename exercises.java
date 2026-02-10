364fde5d74807d07e78dac9b0bbccb1ded987a75
Sameekshats <sameekshats16@gmail.com>
2026-02-10 15:35:31 +0530

    Completed task

---
+++ exercises.java
@@ -0,0 +1,30 @@
+import java.lang.System;
+class Exercise{
+    public static void main(String[] args)
+    {
+        String _name="Sameeksha";
+        int age=20;
+        float height=5.5f;
+        double weight=55.63;
+        System.out.println("NAME: "+_name);
+         System.out.println("AGE: "+age);
+         System.out.println("HEIGHT: "+height);
+         System.out.println("WEIGHT: "+weight);
+        
+    }
+}
+
+class Exercise2{
+    public static void main(String[] args) {
+        int a= 15;
+        int b= 25;
+        int sum= a+b;
+        int diff= a-b;
+        double prod= a*b;
+        double div=a/b;
+        System.out.println("Sum is: "+sum);
+        System.out.println("Difference is: "+diff);
+        System.out.println("Product is: "+prod);
+        System.out.println("Division is: "+div);
+    }
+}
